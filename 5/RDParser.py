ec522bbec8519a95875737c2c84d10e08743947a
dragos231456 <dragos.neghina2000@gmail.com>
2021-11-22 23:59:10 +0200

    g2.txt

--- 5/RDParser.py
+++ 5/RDParser.py
@@ -25,36 +25,36 @@ class RDParser:
                 else:
                     if state.input_stack[0] in self.grammar.nonterminals:
                         nonterminal = state.input_stack[0]
-                        first_production = list(self.grammar.productions[nonterminal][0])
+                        first_production = self.grammar.productions[nonterminal][0].split(' ')
                         state.work_stack.append((nonterminal, first_production))
                         state.input_stack = first_production + state.input_stack[1:]
                     else:
                         if state.index == len(self.input):
                             state.type = StateType.BACK
                         elif state.input_stack[0] == StateType.ERROR:
-                            state.work_stack.append(StateType.ERROR)
+                            state.work_stack.append((StateType.ERROR, []))
                             state.input_stack = state.input_stack[1:]
                         elif state.input_stack[0] == self.input[state.index]:
                             state.index += 1
-                            state.work_stack.append(state.input_stack[0])
+                            state.work_stack.append((state.input_stack[0], []))
                             state.input_stack = state.input_stack[1:]
                         else:
                             state.type = StateType.BACK
             else:
                 if state.type == StateType.BACK:
-                    if state.work_stack[-1] in self.grammar.terminals:
-                        if state.work_stack[-1] == StateType.ERROR:
+                    if state.work_stack[-1][0] in self.grammar.terminals:
+                        if state.work_stack[-1][0] == StateType.ERROR:
                             state.work_stack.pop(-1)
                         else:
                             state.index -= 1
-                            terminal = state.work_stack.pop(-1)
+                            terminal = state.work_stack.pop(-1)[0]
                             state.input_stack = [terminal] + state.input_stack
                     else:
                         (nonterminal, last_production) = state.work_stack[-1]
                         productions = self.grammar.productions[nonterminal]
-                        next_prod = self.__get_next_prod(''.join(last_production), productions)
+                        next_prod = self.__get_next_prod(last_production, productions)
                         if next_prod:
-                            next_prod = list(next_prod)
+                            next_prod = next_prod.split(' ')
                             state.type = StateType.NORMAL
                             state.work_stack.pop(-1)
                             state.work_stack.append((nonterminal, next_prod))
@@ -63,31 +63,57 @@ class RDParser:
                             state.type = StateType.ERROR
                         else:
                             state.work_stack.pop(-1)
-                            if last_production == [StateType.ERROR]:
-                                state.input_stack = list(nonterminal) + state.input_stack
+                            if nonterminal == [StateType.ERROR]:
+                                state.input_stack = [nonterminal] + state.input_stack
                             else:
-                                state.input_stack = list(nonterminal) + state.input_stack[len(last_production):]
+                                state.input_stack = [nonterminal] + state.input_stack[len(last_production):]
 
-        prod_rules = []
         if state.type == StateType.ERROR:
             return False, []
-        else:
-            for prod in state.work_stack:
-                if len(prod) > 1:
-                    if prod[1] in self.grammar.productions[prod[0]]:
-                        prod_rules.append(prod)
-
-        return True, prod_rules
+        return True
 
 
 if __name__ == '__main__':
-    grammar = Grammar.readFromFile('g1.txt')
-    rdp = RDParser(grammar, list("abba"))
-    isValid, seq = rdp.recursive_descent_run()
-    print(isValid)
-
-    if isValid:
-        print("Sequence is valid")
-        print(seq)
-    else:
-        print("Sequence is not valid")
+    grammar = Grammar.readFromFile('g2.txt')
+    rdp = RDParser(grammar, [
+        '{',
+        'int',
+        'a',
+        ';',
+        'int',
+        'b',
+        ';',
+        'int',
+        'c',
+        ';',
+        'read',
+        '(',
+        'a',
+        ')',
+        ';',
+        'read',
+        '(',
+        'b',
+        ')',
+        ';',
+        'read',
+        '(',
+        'c',
+        ')',
+        ';',
+        'if',
+        '(',
+        'a',
+        '>',
+        'b',
+        ')',
+        '{',
+        'print',
+        '(',
+        'a',
+        ')',
+        '}',
+        '}'
+    ])
+    isValid = rdp.recursive_descent_run()
+    print("Sequence is", isValid)
